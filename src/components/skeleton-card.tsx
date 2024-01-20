26abbced809d2cd5ee0c0db08f0b784609a01cec
Web Dev Cody <webdevcody@gmail.com>
2024-01-20 13:11:03 -0500

    image size

--- src/components/skeleton-card.tsx
+++ src/components/skeleton-card.tsx
@@ -4,7 +4,7 @@ export function SkeletonCard() {
   return (
     <Card>
       <CardHeader>
-        <div className="h-32 bg-gray-200 rounded dark:bg-gray-700 mb-2.5"></div>
+        <div className="h-32 md:h-40 lg:h-52 bg-gray-200 rounded dark:bg-gray-700 mb-2.5"></div>
       </CardHeader>
       <CardContent>
         <div className="h-8 bg-gray-200 rounded dark:bg-gray-700 mb-2.5"></div>
