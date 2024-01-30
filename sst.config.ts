bdd47bfd3886141543956b60fe0f32394b8f0dae
Web Dev Cody <webdevcody@gmail.com>
2024-01-29 21:16:51 -0500

    pass env

--- sst.config.ts
+++ sst.config.ts
@@ -15,7 +15,12 @@ export default {
           domainName: "thumbnailcritique.com",
           domainAlias: "www.thumbnailcritique.com",
         },
-        environment: {} as any,
+        environment: {
+          NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+          NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+            process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+          CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+        } as any,
       });
 
       stack.addOutputs({
