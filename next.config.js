d263e07135fa93e06054439dda88220fd6f21a4c
Web Dev Cody <webdevcody@gmail.com>
2024-05-08 18:55:00 -0400

    unoptiized

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    images: {
-      unoptimized: true,
-    },
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: "kindly-kookabura-369.convex.cloud",
