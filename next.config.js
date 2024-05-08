c8cef30806221c2932aad73c620f188f15f2011f
Web Dev Cody <webdevcody@gmail.com>
2024-05-08 18:49:27 -0400

    turn off image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    images: {
+      unoptimized: true,
+    },
     remotePatterns: [
       {
         hostname: "kindly-kookabura-369.convex.cloud",
